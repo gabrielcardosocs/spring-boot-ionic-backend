d7139e08fa6c00a432bf55afb4d0b498251efb07
Gabriel Cardoso <sgabrielcardosoc@gmail.com>
2023-10-22 01:45:58 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/gabrielcardoso/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.gabrielcardoso.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielcardoso.cursomc.domain.Categoria;
+import com.gabrielcardoso.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+
+}
