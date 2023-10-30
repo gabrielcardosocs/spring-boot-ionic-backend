7c03a72406aeb229f0419d2e72f0c36085c3b32e
Gabriel Cardoso <sgabrielcardosoc@gmail.com>
2023-10-30 10:44:42 -0300

    Inserindo uma nova Categoria com o POST

--- src/main/java/com/gabrielcardoso/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/gabrielcardoso/cursomc/resources/CategoriaResources.java
@@ -1,14 +1,17 @@
 package com.gabrielcardoso.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabrielcardoso.cursomc.domain.Categoria;
 import com.gabrielcardoso.cursomc.services.CategoriaService;
@@ -27,4 +30,13 @@ public class CategoriaResources {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
 }
